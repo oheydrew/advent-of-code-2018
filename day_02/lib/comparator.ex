f9d02b57c07ff08d39a8e0d83ce6ab2190d91b25
Drew Noll <drew@myxplor.com>
2018-12-09 16:43:52 +1100

    Rework the problem, create my own compare_strings function rather than using jaro_distance. Neater. 👍

--- day_02/lib/comparator.ex
+++ day_02/lib/comparator.ex
@@ -1,25 +1,37 @@
 defmodule Comparator do
   @moduledoc """
     Compares strings to ultimately find strings that differ by only one letter
-
-    1) Function to take a string, pop the first char off, compare
-    them and
   """
 
-  def checker(_input, _working \\ [], _result \\ [])
-  def checker([], [], result), do: result
-  def checker([_ | tail], [], result) do
-    checker(tail, tail, result)
+  @doc """
+  Recursively goes through a list of strings, checking each against
+  one another using &compare_strings/2 until it finds a string of 25
+  characters, then returns it.
+  """
+  def checker(_input, _working \\ [])
+  def checker([], []), do: nil
+  def checker([_ | tail], []) do
+    checker(tail, tail)
   end
   def checker([input_head | _] = input,
-              [work_head | tail] = _working, result) do
-    jaro = String.jaro_distance(input_head, work_head)
-    current = [jaro, input_head, work_head]
+              [work_head | tail] = _working) do
+    compared = compare_strings(input_head, work_head)
 
-    checker(input, tail, [current | result])
+    case String.length(compared) == 25 do
+      true -> compared
+      false -> checker(input, tail)
+    end
   end
 
-  def get_jaro(results) do
-    Enum.map(results, fn [head | _] -> head end)
+  @doc """
+  Compares 2 strings, and boils them down to a single string containing
+  only their common letters (removes different letters)
+  """
+  def compare_strings(string1, string2) do
+    Enum.zip(String.split(string1, "", trim: true),
+             String.split(string2, "", trim: true))
+    |> Enum.filter(fn {a, b} -> a == b end)
+    |> Enum.map(fn {char, _} -> char end)
+    |> Enum.join("")
   end
 end
